a2041921091fead74cbb3cffbfc5c89aad4f7200
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-12-30 16:19:09 +0000

    feat: add -v version flag to fade-clip and standardize CLI interface

--- src/bin/fade-clip.rs
+++ src/bin/fade-clip.rs
@@ -25,6 +25,10 @@ struct Args {
     #[arg(short = 'o')]
     outfile: Option<String>,
 
+    /// Print version
+    #[arg(short = 'v', long = "version", action = clap::ArgAction::Version)]
+    version: Option<bool>,
+
     /// Print help
     #[arg(short = 'h', long = "help", action = clap::ArgAction::Help)]
     help: Option<bool>,
