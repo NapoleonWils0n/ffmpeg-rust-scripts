8067b1a9725ac4cd86636cf828a1a2d646389590
Daniel J Wilcox <danieljwilcox@gmail.com>
2026-01-01 16:13:37 +0000

    build: update flake.nix to support multi-system builds

--- flake.nix
+++ flake.nix
@@ -8,42 +8,37 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, naersk, rust-overlay }: 
+  outputs = { self, nixpkgs, naersk, rust-overlay, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      system = "x86_64-linux";
+      # system is already provided by eachDefaultSystem, so we don't define it here
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs { inherit system overlays; };
 
-      # Define the toolchain with the IDE components (rust-src and rust-analyzer)
       rustToolchain = pkgs.rust-bin.stable.latest.default.override {
         extensions = [ "rust-src" "rust-analyzer" ];
-        targets = [ "x86_64-unknown-linux-musl" ]; # static Linux builds
+        targets = [ "x86_64-unknown-linux-musl" ]; 
       };
 
-      # Override naersk to use our toolchain
       naerskLib = (naersk.lib.${system}.override {
         cargo = rustToolchain;
         rustc = rustToolchain;
       });
     in {
-      # For 'nix develop'
-      devShells.${system}.default = pkgs.mkShell {
+      devShells.default = pkgs.mkShell {
         buildInputs = [ rustToolchain ];
 
-        # Point tools (like Emacs Eglot) to the Rust source code in the Nix store
         shellHook = ''
           export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
         '';
       };
 
-      # For 'nix build'
-      packages.${system}.default = naerskLib.buildPackage {
+      packages.default = naerskLib.buildPackage {
         src = ./.;
-        # Add these if your Hello World needs them later
-        # buildInputs = [ pkgs.glib ]; 
-        # nativeBuildInputs = [ pkgs.pkg-config ];
       };
-    };
-}
+    } 
+  ); # This closes eachDefaultSystem
+}    # This closes outputs
