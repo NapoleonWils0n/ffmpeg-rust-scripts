e09edf62e418267b14635b8d66b0f188c7bde4e9
Daniel J Wilcox <danieljwilcox@gmail.com>
2026-02-17 21:27:03 +0000

    subtitle-add help

--- src/bin/subtitle-add.rs
+++ src/bin/subtitle-add.rs
@@ -14,9 +14,9 @@ use ffmpeg_rust_scripts::get_media_info;
     author,
     version,
     about = "Add SRT/VTT subtitles to a video as a track you can toggle on and off",
-    after_help = "Example:\n  subtitle-add -i input.mp4 -s input.srt -l eng -o output.mp4\n\n  \
-                  Dependencies:\n  \
-                  ffmpeg: https://www.ffmpeg.org/",
+    after_help = "Example:\n  subtitle-add -i input.mp4 -s input.srt -l eng -o output.mp4\n\n\
+    Dependencies:\n  \
+    ffmpeg: https://www.ffmpeg.org/",
 )]
 #[clap(disable_version_flag = true, disable_help_flag = true)]
 struct Args {
